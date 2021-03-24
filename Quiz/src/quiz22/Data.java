b9ab3f6563719f960342ef86ebcb264b66599d99
user <user@DESKTOP-P2COA7I>
2021-03-24 18:53:20 +0900

    210324

---
+++ Quiz/src/quiz22/Data.java
@@ -0,0 +1,48 @@
+package quiz22;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Data {
+	private String region;
+	private String  size;
+	private String  year;
+	private String  month;
+	private String  prive;
+	
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getPrive() {
+		return prive;
+	}
+	public void setPrive(String prive) {
+		this.prive = prive;
+	}
+	
+	
+	
+	
+}
